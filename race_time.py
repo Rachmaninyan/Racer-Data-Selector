7439a65a3e2326251c393b14534335cc52028bb5
Rachmaninyan <103300627+Rachmaninyan@users.noreply.github.com>
2024-10-20 20:16:32 -0700

    Add files via upload

---
+++ race_time.py
@@ -0,0 +1,99 @@
+class RaceTime:
+    """ RaceTime: represents a race time result in 
+    milliseconds (ms), seconds (sec), minutes (mins)
+    Precondition, ms, sec, mins>=0, ms<1000, sec<60
+    """
+
+    def __init__(self, ms: int, sec: int, mins: int) -> None:
+        """ initializes attributes of RaceTime instance
+        >>> rt = RaceTime(88, 20, 4)
+        """
+        self.__ms = ms
+        self.__sec = sec
+        self.__mins = mins
+
+    def get_ms(self) -> int:
+        """ returns ms of self RaceTime instance
+        >>> rt = RaceTime(88, 20, 4)
+        >>> rt.get_ms()
+        88
+        """
+        return self.__ms
+
+    def get_sec(self) -> int:
+        """ returns sec of self RaceTime instance
+        >>> rt = RaceTime(88, 20, 4)
+        >>> rt.get_sec()
+        20
+        """
+        return self.__sec
+
+    def get_mins(self) -> int:
+        """ returns mins of self RaceTime instance
+        >>> rt = RaceTime(88, 20, 4)
+        >>> rt.get_mins()
+        4
+        """
+        return self.__mins
+
+    def __str__(self) -> str:
+        """ returns a readable string with ms, sec, mins of RaceTime
+        >>> rt = RaceTime(88, 20, 4)
+        >>> str(rt)
+        '4:20:88'
+        """
+        return f'{self.__mins}:{self.__sec}:{self.__ms}'
+
+    def __repr__(self) -> str:
+        """ returns a string representation of self RaceTime
+        >>> rt = RaceTime(88, 20, 4)
+        >>> repr(rt)
+        'RaceTime(88, 20, 4)'
+        """
+        return f'RaceTime({self.__ms}, {self.__sec}, {self.__mins})'
+
+    def __eq__(self, other: 'RaceTime') -> bool:
+        """ returns True if self RaceTime has same ms, sec, mins as
+        other RaceTime, otherwise False
+        >>> rt121920a = RaceTime(12, 19, 20)
+        >>> rt121920b = RaceTime(12, 19, 20)
+        >>> rt121921  = RaceTime(12, 19, 21)
+        >>> rt121820  = RaceTime(12, 18, 20)
+        >>> rt111920  = RaceTime(11, 19, 20)
+
+        >>> rt121920a == rt121920a
+        True
+        >>> rt121920a == rt121920b
+        True
+        >>> rt121920a == rt121921
+        False
+        >>> rt121920a == rt121820
+        False
+        >>> rt121920a == rt111920
+        False
+        """
+        return (self.__ms == other.get_ms()
+                and self.__sec == other.get_sec()
+                and self.__mins == other.get_mins())
+    
+    def is_faster(self, other: 'RaceTime')->bool:
+        '''
+        Compares the instance of RaceTime with another instance
+        and returns a boolean value representing if the self instance
+        is faster than the second RaceTime instance.
+        '''
+        #get time of self
+        selfmins = self.get_mins()
+        selfsecs = self.get_sec() if len(str(self.get_sec()))==2 else f'0{self.get_sec()}'
+        selfms = self.get_ms()
+        while len(str(selfms))<3:
+            selfms = '0'+str(selfms)
+        selftime = int(f'{selfmins}{selfsecs}{selfms}')
+        #get time of other
+        othermins = other.get_mins()
+        othersecs = other.get_sec() if len(str(other.get_sec()))==2 else f'0{other.get_sec()}'
+        otherms = other.get_ms()
+        while len(str(otherms))<3:
+            otherms = '0'+str(otherms)
+        othertime = int(f'{othermins}{othersecs}{otherms}')
+        return selftime<othertime
